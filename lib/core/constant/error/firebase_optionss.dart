f86427413c3931625f87e56f9b24e06bd598198a
Ayush soni <ayush.soni84080@gmail.com>
2025-02-14 21:43:04 +0530

    Push Notification

---
+++ lib/core/constant/error/firebase_optionss.dart
@@ -0,0 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
+
+final FirebaseOptions option = FirebaseOptions(
+    apiKey: "AIzaSyC9XKkMdz0Jlh06go0ICtGkEWkCF9jCD7Y",
+    appId: "1:1033636986652:android:6b59d542d4808d0922c4d9",
+    messagingSenderId: "1033636986652",
+    projectId: "incomeexpancetracker",
+    storageBucket: "incomeexpancetracker.firebasestorage.app",
+);
--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,19 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get_it/get_it.dart';
+import 'package:income_expance_tracker_app/core/constant/error/firebase_optionss.dart';
 import 'package:income_expance_tracker_app/feature/presentation/blocs/nav/navigation_bloc.dart';
 import 'package:income_expance_tracker_app/feature/presentation/blocs/password/toggle_bloc.dart';
 import 'package:income_expance_tracker_app/getit_dependency.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 import 'core/constant/routes.dart';
 import 'core/theme/theme.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: option,
+  );
   setUpLocator();
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent));
   runApp(MultiBlocProvider(providers: [
